ff81547e117b6537f588c5aa722730cd9e291f46
zenro1232 <126719730+zenro1232@users.noreply.github.com>
2023-07-03 21:32:31 +0300

    Add files via upload

---
+++ src/burçbulm.java
@@ -0,0 +1,153 @@
+import java.util.Scanner;
+public class burçbulm {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int  gün,ay;
+        String burc;
+System.out.println("LÜtfen dogdugunuz ayı  seçiniz :");
+ay = inp.nextInt();
+        System.out.println("LÜtfen dogdugunuz Günü seçiniz :");
+        gün =inp.nextInt();
+
+        if(1<=ay &&  ay<=12){
+            if (ay==1){
+                if (1<=gün && gün<=31){
+                if (gün <= 21){
+                burc = "Oğlak";
+
+                }else{
+                    burc = "kova";
+                }
+        }
+                if(1<=ay &&  ay<=12){
+                    if (ay==2){
+                        if (1<=gün && gün<=31){
+                            if (gün <= 21){
+                                burc = "Oğlak";
+
+                            }else{
+                                burc = "kova";
+                                if(1<=ay &&  ay<=12){
+                                    if (ay==3){
+                                        if (1<=gün && gün<=31){
+                                            if (gün <= 21){
+                                                burc = "Oğlak";
+
+                                            }else{
+                                                burc = "kova";
+                                                if(1<=ay &&  ay<=12){
+                                                    if (ay==4){
+                                                        if (1<=gün && gün<=31){
+                                                            if (gün <= 21){
+                                                                burc = "Oğlak";
+
+                                                            }else{
+                                                                burc = "kova";
+                                                                if(1<=ay &&  ay<=12){
+                                                                    if (ay==5){
+                                                                        if (1<=gün && gün<=31){
+                                                                            if (gün <= 21){
+                                                                                burc = "Oğlak";
+
+                                                                            }else{
+                                                                                burc = "kova";
+                                                                                if(1<=ay &&  ay<=12){
+                                                                                    if (ay==6){
+                                                                                        if (1<=gün && gün<=31){
+                                                                                            if (gün <= 21){
+                                                                                                burc = "Oğlak";
+
+                                                                                            }else{
+                                                                                                burc = "kova";
+                                                                                                if(1<=ay &&  ay<=12){
+                                                                                                    if (ay==7){
+                                                                                                        if (1<=gün && gün<=31){
+                                                                                                            if (gün <= 21){
+                                                                                                                burc = "Oğlak";
+
+                                                                                                            }else{
+                                                                                                                burc = "kova";
+                                                                                                                if(1<=ay &&  ay<=12){
+                                                                                                                    if (ay==8){
+                                                                                                                        if (1<=gün && gün<=31){
+                                                                                                                            if (gün <= 21){
+                                                                                                                                burc = "Oğlak";
+
+                                                                                                                            }else{
+                                                                                                                                burc = "kova";
+                                                                                                                                if(1<=ay &&  ay<=12){
+                                                                                                                                    if (ay==9){
+                                                                                                                                        if (1<=gün && gün<=31){
+                                                                                                                                            if (gün <= 21){
+                                                                                                                                                burc = "Oğlak";
+
+                                                                                                                                            }else{
+                                                                                                                                                burc = "kova";
+                                                                                                                                                if(1<=ay &&  ay<=12){
+                                                                                                                                                    if (ay==10){
+                                                                                                                                                        if (1<=gün && gün<=31){
+                                                                                                                                                            if (gün <= 21){
+                                                                                                                                                                burc = "Oğlak";
+
+                                                                                                                                                            }else{
+                                                                                                                                                                burc = "kova";
+                                                                                                                                                                if(1<=ay &&  ay<=12){
+                                                                                                                                                                    if (ay==11){
+                                                                                                                                                                        if (1<=gün && gün<=31){
+                                                                                                                                                                            if (gün <= 21){
+                                                                                                                                                                                burc = "Oğlak";
+
+                                                                                                                                                                            }else{
+                                                                                                                                                                                burc = "kova";if(1<=ay &&  ay<=12){
+                                                                                                                                                                                    if (ay==12){
+                                                                                                                                                                                        if (1<=gün && gün<=31){
+                                                                                                                                                                                            if (gün <= 21){
+                                                                                                                                                                                                burc = "Oğlak";
+
+                                                                                                                                                                                            }else{("burc");                                                                                                                                                                                        burc = "kova";
+}
+            }
+        }
+} }
+                                                                                                                                                                        }
+                                                                                                                                                                    }
+                                                                                                                                                                }
+                                                                                                                                                            }
+                                                                                                                                                        }
+                                                                                                                                                    }
+                                                                                                                                                }
+                                                                                                                                            }
+                                                                                                                                        }
+                                                                                                                                    }
+                                                                                                                                }
+                                                                                                                            }
+                                                                                                                        }
+                                                                                                                    }
+
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+
+                                                                        }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }}
+                }
+            }
+    }
+}}}}
